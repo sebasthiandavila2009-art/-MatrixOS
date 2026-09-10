d7b1ec0866c95894c7422d91c9e767d46aab0a64
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-10 19:25:32 -0400

    Update bootloader to version 0.2 with enhancements

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,19 +1,49 @@
 ; MatrixOS Bootloader
-; Version 0.1
+; Version 0.2
 
 BITS 16
 ORG 0x7C00
 
 start:
     cli
+
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
-    sti
 
-    mov si, message
+    mov [boot_drive], dl
+
+    ; Load kernel from disk
+    mov ah, 0x02
+    mov al, 16
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
+    mov bx, 0x1000
+    int 0x13
+
+    jc disk_error
+
+    ; Enable A20
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    ; Load GDT
+    lgdt [gdt_descriptor]
+
+    ; Enter protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp 0x08:protected_mode
+
+disk_error:
+    mov si, error_message
 
 .print:
     lodsb
@@ -21,19 +51,55 @@ start:
     jz .halt
 
     mov ah, 0x0E
-    mov bh, 0x00
     int 0x10
     jmp .print
 
 .halt:
     cli
-
-.wait:
     hlt
-    jmp .wait
+    jmp .halt
 
-message db 'MATRIXOS v0.1', 13, 10
-        db 'Bootloader started.', 13, 10, 0
+BITS 32
+
+protected_mode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov esp, 0x90000
+
+    jmp 0x08:0x1000
+
+boot_drive db 0
+error_message db 'MATRIXOS: Disk error', 0
+
+gdt_start:
+
+gdt_null:
+    dq 0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 10011010b
+    db 11001111b
+    db 0
+
+gdt_data:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 10010010b
+    db 11001111b
+    db 0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
