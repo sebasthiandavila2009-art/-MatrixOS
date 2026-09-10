d775e40c5ec152252fc8e8c3e5dfb47b04c698e2
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-10 19:24:47 -0400

    Update kernel version and implement message display
    Updated kernel version to 0.2 and added message display functionality.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,19 @@
 // MatrixOS Kernel
-// Version 0.1
+// Version 0.2
 
 void kernel_main(void)
 {
-    // MatrixOS kernel entry point.
-    // Hardware initialization will be added here.
+    volatile unsigned short *video = (unsigned short *)0xB8000;
+
+    const char *message = "MATRIXOS v0.2 - Kernel Started";
+
+    for (int i = 0; message[i] != '\0'; i++)
+    {
+        video[i] = (unsigned short)message[i] | 0x0700;
+    }
+
+    while (1)
+    {
+        __asm__ volatile ("hlt");
+    }
 }
