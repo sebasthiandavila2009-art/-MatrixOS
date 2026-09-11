1e99fd7f91ce36277881609115bfe0296d075252
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-11 08:59:14 -0400

    Update bootloader to version 0.7 with comments

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,5 @@
 ; MatrixOS Bootloader
-; Version 0.6
+; Version 0.7
 
 BITS 16
 ORG 0x7C00
@@ -15,11 +15,11 @@ start:
 
     mov [boot_drive], dl
 
-    ; Boot message
+    ; Bootloader message
     mov si, boot_message
     call print_string
 
-    ; Load kernel from sectors 2 and 3
+    ; Load kernel: 2 sectors -> physical address 0x1000
     mov ah, 0x02
     mov al, 2
     mov ch, 0
@@ -43,12 +43,12 @@ start:
     ; Load GDT
     lgdt [gdt_descriptor]
 
-    ; Enter protected mode
+    ; Enable protected mode
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    ; Explicit 32-bit far jump
+    ; 32-bit far jump
     db 0x66
     db 0xEA
     dd protected_mode
@@ -82,33 +82,45 @@ disk_error:
 
 
 ; =========================================
-; PROTECTED MODE
+; 32-BIT PROTECTED MODE
 ; =========================================
 
 BITS 32
 
 protected_mode:
 
-    ; Load data segments
+    ; Reload data segments
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
 
-    ; Protected-mode stack
+    ; Set 32-bit stack
     mov esp, 0x90000
 
-    ; Diagnostic: MRIDE
-    mov word [0xB8000], 0x074D
-    mov word [0xB8002], 0x0752
-    mov word [0xB8004], 0x0749
-    mov word [0xB8006], 0x0744
-    mov word [0xB8008], 0x0745
+    ; Write a visible protected-mode message
+    mov edi, 0xB8000
 
-    ; Jump to kernel
-    db 0xEA
-    dd 0x1000
-    dw 0x08
+    mov ax, 0x074D
+    mov [edi], ax
+
+    mov ax, 0x0752
+    mov [edi + 2], ax
+
+    mov ax, 0x0749
+    mov [edi + 4], ax
+
+    mov ax, 0x0744
+    mov [edi + 6], ax
+
+    mov ax, 0x0745
+    mov [edi + 8], ax
+
+    ; Stay here for now
+.hang:
+    cli
+    hlt
+    jmp .hang
 
 
 ; =========================================
@@ -133,24 +145,24 @@ error_message:
 
 gdt_start:
 
-gdt_null:
+    ; Null descriptor
     dq 0
 
-gdt_code:
+    ; Code segment
     dw 0xFFFF
-    dw 0
-    db 0
+    dw 0x0000
+    db 0x00
     db 10011010b
     db 11001111b
-    db 0
+    db 0x00
 
-gdt_data:
+    ; Data segment
     dw 0xFFFF
-    dw 0
-    db 0
+    dw 0x0000
+    db 0x00
     db 10010010b
     db 11001111b
-    db 0
+    db 0x00
 
 gdt_end:
 
