fbbc6ff959eb0a25876ed306bea2a701f9e8debc
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-10 22:49:37 -0400

    Update kernel version to 0.4 and improve cursor handling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,19 +1,32 @@
 // MatrixOS Kernel
-// Version 0.2
+// Version 0.4
+
+extern char keyboard_get_char(void);
+
+volatile unsigned short *video = (unsigned short *)0xB8000;
+
+int cursor = 0;
 
 void kernel_main(void)
 {
-    volatile unsigned short *video = (unsigned short *)0xB8000;
-
-    const char *message = "MATRIXOS v0.2 - Kernel Started";
+    const char *message = "MATRIXOS v0.4 - Shell Ready";
 
     for (int i = 0; message[i] != '\0'; i++)
     {
-        video[i] = (unsigned short)message[i] | 0x0700;
+        video[cursor++] = (unsigned short)message[i] | 0x0700;
     }
+
+    cursor++;
 
     while (1)
     {
-        __asm__ volatile ("hlt");
+        char key = keyboard_get_char();
+
+        video[cursor++] = (unsigned short)key | 0x0700;
+
+        if (cursor >= 2000)
+        {
+            cursor = 0;
+        }
     }
 }
