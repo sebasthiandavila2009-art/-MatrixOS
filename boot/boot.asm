7b4aaba06cefa59657f9141fe005c499d5f3201b
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-10 23:03:47 -0400

    Update bootloader version and improve comments

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,5 @@
 ; MatrixOS Bootloader
-; Version 0.2 Debug
+; Version 0.4
 
 BITS 16
 ORG 0x7C00
@@ -15,11 +15,11 @@ start:
 
     mov [boot_drive], dl
 
-    ; Debug: bootloader is running
+    ; Bootloader message
     mov si, boot_message
     call print_string
 
-    ; Load kernel from sector 2
+    ; Load kernel: sectors 2 and 3
     mov ah, 0x02
     mov al, 2
     mov ch, 0
@@ -31,13 +31,13 @@ start:
 
     jc disk_error
 
-    ; Debug: kernel loaded
+    ; Kernel loaded
     mov si, kernel_message
     call print_string
 
     ; Enable A20
     in al, 0x92
-    or al, 2
+    or al, 00000010b
     out 0x92, al
 
     ; Load GDT
@@ -48,7 +48,9 @@ start:
     or eax, 1
     mov cr0, eax
 
+    ; Far jump into protected mode
     jmp 0x08:protected_mode
+
 
 print_string:
 .next:
@@ -59,32 +61,39 @@ print_string:
     mov ah, 0x0E
     mov bh, 0
     int 0x10
+
     jmp .next
 
 .done:
     ret
 
+
 disk_error:
     mov si, error_message
     call print_string
 
-.halt:
+.hang:
     cli
     hlt
-    jmp .halt
+    jmp .hang
+
 
 BITS 32
 
 protected_mode:
+
+    ; Data segment
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
 
+    ; Stack
     mov esp, 0x90000
 
-    ; Jump to C kernel
+    ; Jump to kernel entry
     jmp 0x08:0x1000
+
 
 boot_drive db 0
 
@@ -96,6 +105,11 @@ kernel_message:
 
 error_message:
     db 'MATRIXOS: Disk error', 13, 10, 0
+
+
+; =========================
+; GDT
+; =========================
 
 gdt_start:
 
@@ -123,6 +137,7 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
+
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
