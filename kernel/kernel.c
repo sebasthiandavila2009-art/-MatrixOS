6bfe998b017380f22d8eab40274f069dc1452f57
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-11 14:57:40 -0400

    Update kernel version to 0.5 and initialize mouse

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,8 @@
 // MatrixOS Kernel
-// Version 0.4
+// Version 0.5
 
 extern char keyboard_get_char(void);
+extern void mouse_init(void);
 
 volatile unsigned short *video = (unsigned short *)0xB8000;
 
@@ -9,8 +10,12 @@ int cursor = 0;
 
 void kernel_main(void)
 {
-    const char *message = "MATRIXOS v0.4 - SHELL READY";
-    
+    const char *message = "MATRIXOS v0.5 - MOUSE READY";
+
+    /* Initialize the mouse */
+    mouse_init();
+
+    /* Display startup message */
     for (int i = 0; message[i] != '\0'; i++)
     {
         video[cursor++] = (unsigned short)message[i] | 0x0700;
@@ -18,6 +23,7 @@ void kernel_main(void)
 
     cursor++;
 
+    /* Keyboard loop */
     while (1)
     {
         char key = keyboard_get_char();
