986bbbf2391b885e23304738f5f60e70d3287994
Chris Davila <chrisdavila2026@Chriss-MacBook-Air.local>
2026-09-11 19:37:25 -0400

    Update bootloader to load 12 kernel sectors

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,5 @@
 ; MatrixOS Bootloader
-; Version 3.2 - 12 Sector Kernel
+; Version 3.0 - 10 Sector Kernel
 
 BITS 16
 ORG 0x7C00
@@ -23,16 +23,14 @@ start:
 
     ; --------------------------------
     ; Load MatrixOS kernel
-    ; 12 sectors
-    ; Kernel starts at sector 2
-    ; Loaded to physical address 0x1000
+    ; 10 sectors -> physical 0x1000
     ; --------------------------------
 
     mov ah, 0x02
-    mov al, 12
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
+    mov al, 0x0B
+    mov ch, 0x00
+    mov cl, 0x02
+    mov dh, 0x00
     mov dl, [boot_drive]
     mov bx, 0x1000
 
@@ -63,13 +61,13 @@ start:
     or eax, 0x01
     mov cr0, eax
 
-    ; Jump to 32-bit protected mode
+    ; Jump to protected mode
     jmp 0x08:protected_mode
 
 
-; ================================================
-; BIOS Text Output
-; ================================================
+; --------------------------------
+; BIOS text output
+; --------------------------------
 
 print_string:
     lodsb
@@ -87,9 +85,9 @@ print_string:
     ret
 
 
-; ================================================
-; Disk Error
-; ================================================
+; --------------------------------
+; Disk error
+; --------------------------------
 
 disk_error:
     mov si, error_message
@@ -112,4 +110,68 @@ protected_mode:
     mov ax, 0x10
 
     mov ds, ax
-    mov es
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, 0x90000
+
+    ; Jump to MatrixOS kernel
+    jmp 0x08:0x1000
+
+
+; ================================================
+; Global Descriptor Table
+; ================================================
+
+gdt_start:
+
+    dq 0
+
+gdt_code:
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+
+gdt_data:
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+
+; ================================================
+; Variables / Messages
+; ================================================
+
+boot_drive:
+    db 0
+
+boot_message:
+    db "MATRIXOS: Bootloader OK", 13, 10, 0
+
+kernel_message:
+    db "MATRIXOS: Kernel loaded", 13, 10, 0
+
+error_message:
+    db "MATRIXOS: Disk error", 13, 10, 0
+
+
+; Boot sector must be exactly 512 bytes
+times 510 - ($ - $$) db 0
+
+dw 0xAA55
