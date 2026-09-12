52a4096815b2f9a8796362e0bba05dd928baf0bf
sebasthiandavila2009-art <sebasthiandavila2009@gmail.com>
2026-09-12 00:41:41 -0400

    Upgrade bootloader to version 1.6
    Updated bootloader to version 1.6 with improved error handling and diagnostics.

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,5 @@
 ; MatrixOS Bootloader
-; Version 1.5 - Reliable 30 Sector LBA Loader
+; Version 1.6 - Boot Diagnostic
 
 BITS 16
 ORG 0x7C00
@@ -15,40 +15,67 @@ start:
 
     mov [boot_drive], dl
 
+    ; A = bootloader started
+    mov al, 'A'
+    call print_char
+
     ; Enable A20
     in al, 0x92
-    or al, 00000010b
+    or al, 2
     out 0x92, al
 
-    ; Check BIOS Extended Disk Services
+    ; Check LBA support
     mov ah, 0x41
     mov bx, 0x55AA
     mov dl, [boot_drive]
-
     int 0x13
-    jc disk_error
+    jc error
 
     cmp bx, 0xAA55
-    jne disk_error
+    jne error
 
     test cx, 1
-    jz disk_error
+    jz error
 
-    ; -----------------------------------------
-    ; READ 1
-    ; 16 sectors
-    ; LBA 1
-    ; Destination: 0000:1000
-    ; -----------------------------------------
+    ; B = LBA support works
+    mov al, 'B'
+    call print_char
 
+    ; Read kernel
     mov si, dap1
     mov dl, [boot_drive]
     mov ah, 0x42
-
     int 0x13
-    jnc read2
+    jc retry
 
-    ; Reset and retry
+    ; C = first read works
+    mov al, 'C'
+    call print_char
+
+    ; Read remaining kernel space
+    mov si, dap2
+    mov dl, [boot_drive]
+    mov ah, 0x42
+    int 0x13
+    jc retry2
+
+    ; D = second read works
+    mov al, 'D'
+    call print_char
+
+    ; Enter protected mode
+    cli
+    lgdt [gdt_descriptor]
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; E = protected mode entry
+    jmp 0x08:protected_mode
+
+
+retry:
     xor ah, ah
     mov dl, [boot_drive]
     int 0x13
@@ -57,41 +84,21 @@ start:
     mov dl, [boot_drive]
     mov ah, 0x42
     int 0x13
-    jc disk_error
+    jc error
 
-
-read2:
-
-    ; -----------------------------------------
-    ; READ 2
-    ; 14 sectors
-    ; LBA 17
-    ; Destination: 0000:3000
-    ; -----------------------------------------
-
-    mov si, dap2
-    mov dl, [boot_drive]
-    mov ah, 0x42
-
-    int 0x13
-    jnc protected
-
-    ; Reset and retry
-    xor ah, ah
-    mov dl, [boot_drive]
-    int 0x13
+    mov al, 'C'
+    call print_char
 
     mov si, dap2
     mov dl, [boot_drive]
     mov ah, 0x42
     int 0x13
-    jc disk_error
+    jc error
 
-
-protected:
+    mov al, 'D'
+    call print_char
 
     cli
-
     lgdt [gdt_descriptor]
 
     mov eax, cr0
@@ -101,26 +108,50 @@ protected:
     jmp 0x08:protected_mode
 
 
-disk_error:
+retry2:
+    xor ah, ah
+    mov dl, [boot_drive]
+    int 0x13
 
+    mov si, dap2
+    mov dl, [boot_drive]
+    mov ah, 0x42
+    int 0x13
+    jc error
+
+    mov al, 'D'
+    call print_char
+
+    cli
+    lgdt [gdt_descriptor]
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp 0x08:protected_mode
+
+
+error:
     mov si, error_message
 
-print_error:
-
+error_loop:
     lodsb
-
     test al, al
     jz halt
 
+    call print_char
+    jmp error_loop
+
+
+print_char:
     mov ah, 0x0E
     mov bh, 0
     int 0x10
-
-    jmp print_error
+    ret
 
 
 halt:
-
     cli
     hlt
     jmp halt
@@ -131,7 +162,6 @@ BITS 32
 protected_mode:
 
     mov ax, 0x10
-
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -145,90 +175,58 @@ protected_mode:
 
 BITS 16
 
-; -----------------------------------------
-; GDT
-; -----------------------------------------
-
 gdt_start:
-
     dq 0
 
 gdt_code:
     dw 0xFFFF
-    dw 0x0000
-    db 0x00
+    dw 0
+    db 0
     db 10011010b
     db 11001111b
-    db 0x00
+    db 0
 
 gdt_data:
     dw 0xFFFF
-    dw 0x0000
-    db 0x00
+    dw 0
+    db 0
     db 10010010b
     db 11001111b
-    db 0x00
+    db 0
 
 gdt_end:
 
-
 gdt_descriptor:
-
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
 
-; -----------------------------------------
-; DAP 1
-; 16 sectors
-; LBA 1
-; 0000:1000
-; -----------------------------------------
-
 align 4
 
 dap1:
-
     db 0x10
-    db 0x00
-
+    db 0
     dw 16
-
     dw 0x1000
-    dw 0x0000
-
+    dw 0
     dq 1
 
-
-; -----------------------------------------
-; DAP 2
-; 14 sectors
-; LBA 17
-; 0000:3000
-; -----------------------------------------
 
 align 4
 
 dap2:
-
     db 0x10
-    db 0x00
-
+    db 0
     dw 14
-
     dw 0x3000
-    dw 0x0000
-
+    dw 0
     dq 17
 
 
 boot_drive:
-
     db 0
 
-
 error_message:
-
     db "MatrixOS: Disk error", 0
 
 
